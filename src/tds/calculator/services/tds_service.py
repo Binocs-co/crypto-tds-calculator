b3cc8d4273919b31434fde65c00e69c0945cb670
Pankaj Garg <garg.pankaj83@gmail.com>
2022-06-24 23:03:35 +0530

    added few models to comply with the request response

--- src/tds/calculator/services/tds_service.py
+++ src/tds/calculator/services/tds_service.py
@@ -6,25 +6,37 @@ from fastapi.encoders import jsonable_encoder
 
 from tds.calculator.models.binocs_model import BinocsId
 from tds.calculator.models.user import User
+from tds.calculator.models.trade import UserTradeDetail, TradeDetail, Amount
 
 logger = get_logger(__name__)
-
 
 class TDSService:
     #TODO: use exchange_id for interaction with the binocs servers here (if any)
     #TODO: We should use the private key generated from the partner dashboard for secure
     #      communication
-    def __init__(self, exchange_id: str, private_key: str, escrow_account, data_store):
-        pass
+    def __init__(self, exchange_id: str, private_key: str, escrow_account, datastore):
+        self.datastore = datastore
+        self.escrow_account = escrow_account
+        self.private_key = private_key
+        self.exchange_id = exchange_id
 
     async def registerUser(self, user: User):
-        #TODO: Securely register user on Binocs as well
         logger.info("registering user with user body {}".format(user))
-        return BinocsId(id = generate_unique_account_id())
 
-    async def tdsValue(self, trade: UserTradeDetails):
-        buyer = get_user(buyer_id) #TODO: @shakun, we will need these functions
-        seller = get_user(seller_id) #TODO: @shakun, we will need these functions
+        binocs_id = BinocsId(id = generate_unique_account_id())
+        self.datastore.save_user(binocs_id, user)
+        #TODO: Securely register user on Binocs as well
+        return binocs_id
+
+    '''
+    tds | [{exchange_user_id: amount: [value: int, coin: string, decimal: int]}]
+    binocs-account-details | account details in the exchange [TBD]
+
+    '''
+    async def tdsValue(self, trade: UserTradeDetail):
+        buyer = get_user(trade.buyer_id) #TODO: @shakun, we will need these functions
+        seller = get_user(trade.seller_id) #TODO: @shakun, we will need these functions
+        trade_details = trade.details
 
         #Buyer: Resident - Seller: Resident
         if buyer.is_resident('IN') and seller.is_resident('IN'):
@@ -41,7 +53,7 @@ class TDSService:
 
         return await self.mongo_util.get({"user_id": user_id})
 
-    async def getTDSList(self, account_flag, page, limit):
+    async def getTDSStatus(self, account_flag, page, limit):
         document_filter = {}
         extra_args = {}
         if account_flag:
