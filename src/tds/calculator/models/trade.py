ad5c53ad01777e4852178fb29b5801e12cbf2617
Pankaj Garg <garg.pankaj83@gmail.com>
2022-06-24 23:48:19 +0530

    added few models to comply with the request response

--- src/tds/calculator/models/trade.py
+++ src/tds/calculator/models/trade.py
@@ -3,20 +3,21 @@ from tds.calculator.models.binocs_model import BinocsModel
 from tds.calculator.models.user import User
 
 class UserTradeDetail(BaseModel):
-    ''' maker: User | mandatory | exchange-id of the maker
-        taker: User | mandatory | exchange-id of the taker
+    '''
         trade_id: string | mandatory
-        trade_type: string | mandatory | buy OR sell
         timestamp: int | mandatory
+        trade_type: string | mandatory | 'TRADE' for now
+        maker: User | mandatory | exchange-id of the maker
         maker_amount: list | mandatory | [Amount]
+        taker: User | mandatory | exchange-id of the taker
         taker_amount: list | mandatory | [Amount]
     '''
-    maker: User = Field(None)
-    taker: User = Field(None)
     trade_id: str = Field(None)
-    trade_type: str = Field(None)
     timestamp: int = Field(None)
+    trade_type: str = Field(None)
+    maker: User = Field(None)
     maker_amount: list = Field(None)
+    taker: User = Field(None)
     taker_amount: list = Field(None)
 
 class Amount(BaseModel):
